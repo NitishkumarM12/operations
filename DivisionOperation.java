650a3a62a53e19b920be2b4809bf612307f24b74
Nitishkumar <mudaraddinitishkumar@outlook.com>
2025-05-06 15:57:36 +0530

    Update DivisionOperation.java

--- DivisionOperation.java
+++ DivisionOperation.java
@@ -2,7 +2,7 @@
 public class DivisionOperation {
     public static void main(String[] args) {
         int a = 10;
-        int b = 0;
+        int b = 20;
         try {
             int result = a / b;
             System.out.println("Result: " + result);
