811eb225d102cbd0af5398185bd712262be9db0b
NitishkumarM12 <mudaraddinitishkumar@outlook.com>
2025-05-06 15:47:01 +0530

    save file

---
+++ BasicOperation.java
@@ -0,0 +1,16 @@
+package student;
+
+public class BasicOperation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+}
+}
